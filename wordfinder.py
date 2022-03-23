665a71d0f9e287fc37be7ac7953f66c392dd6e24
Maurice <maurice.baez1@gmail.com>
2022-03-23 07:55:19 -0700

    completed exercise

--- wordfinder.py
+++ wordfinder.py
@@ -1,33 +1,33 @@
+from random import choice
+
 class WordFinder:
 
     """Word Finder: finds random words from a dictionary."""
 
-    def __init__(self, file_address):
-        self.file_address = file_address
-        self.word_list = self.open_and_read_file()
-        self.words_read()
+    def __init__(self, path):
 
-    def open_and_read_file(self):
-        return open(self.file_address).readlines()
+        file = open(path)
 
-    def words_read(self):
+        self.word_list = self.parse(file)
+
         print(f"{len(self.word_list)} words read")
 
+
+    def parse(self, file):
+
+       return [line.strip() for line in file]
+
+
     def random(self):
-        from random import choice
-        return choice(self.word_list).removesuffix("\n")
+
+        return choice(self.word_list)
 
 
 class SpecialWordFinder(WordFinder):
 
+    def parse(self, file):
 
-    def __init__(self, file_address):
-        super().__init__(file_address)
-        self.word_list = self.words_list()
-
-
-    def words_list(self):
-       return [word for word in self.word_list if not word.startswith("#") and not word.startswith("\n")]
+      return [word for word in super().parse(file) if word != "" and not word.startswith("#")]
 
 
 
