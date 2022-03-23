022296138e8ad9ca0ce38d075449fedff3fec5d8
Maurice <maurice.baez1@gmail.com>
2022-03-22 19:19:28 -0700

    worked on subclass

--- wordfinder.py
+++ wordfinder.py
@@ -1,29 +1,33 @@
 class WordFinder:
+
     """Word Finder: finds random words from a dictionary."""
+
     def __init__(self, file_address):
         self.file_address = file_address
         self.word_list = self.open_and_read_file()
-        print(self.words_read())
+        self.words_read()
 
-
-
-#opening up the file 
     def open_and_read_file(self):
-        return open(self.file_address).readlines() #returns array
-        
-        
+        return open(self.file_address).readlines()
 
     def words_read(self):
-        return f"{len(self.word_list)} words read"
+        print(f"{len(self.word_list)} words read")
 
     def random(self):
         from random import choice
         return choice(self.word_list).removesuffix("\n")
 
 
-"""def random(self):
+class SpecialWordFinder(WordFinder):
 
 
-file = open("random_words.txt")
-"""
+    def __init__(self, file_address):
+        super().__init__(file_address)
+        self.word_list = self.words_list()
+
+
+    def words_list(self):
+       return [word for word in self.word_list if not word.startswith("#") and not word.startswith("\n")]
+
+
 
